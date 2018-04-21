f155baf6d3321b5eab3efb8005bc66413348197e
CrazyLithuanian <kestutis.stugys@gmail.com>
2018-04-21 18:44:50 +0100

    All comments are in place

--- media.py
+++ media.py
@@ -1,19 +1,26 @@
 import webbrowser
 class Movie():
     def __init__(self, title, storyline, poster_image, trailer_youtube):
+        """ The initializer initializes movie title, storyline, poster image url and trailer url from youtube,
+            these must be supplied while initializing the class"""
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # ================ These functions are mostly used for debugging purposes mostly ===================================
     def print_title(self):
+        """Prints the title of the movie"""
         print(self.title)
 
     def print_storyline(self):
+        """Print the storyline"""
         print(self.storyline)
 
     def show_poster(self):
+        """Opens poster in the browser"""
         webbrowser.open(self.poster_image_url)
 
     def show_trailer(self):
+        """Opens trailer in the browser"""
         webbrowser.open(self.trailer_youtube_url)
